2b3458c5f51ef10b3be8155176430d05636075e2
Alex K <askrasn@gmail.com>
2021-05-05 14:53:41 -0700

    + add solutions to rotatematrix wip

--- leet_code/rotate2dMatrix.js
+++ leet_code/rotate2dMatrix.js
@@ -35,7 +35,6 @@ const inputMatrix =
 //         [13, 3, 6, 7],
 //         [15, 14, 12, 16]
 //     ];
-
 // rotate the input matrix in-place such that it becomes:
 // [
 //   [15,13, 2, 5],
@@ -44,32 +43,102 @@ const inputMatrix =
 //   [16, 7,10,11]
 // ]
 
+//* Example 3:
+// const inputMatrix = [[1]]
+// Output: [[1]]
+//* Example 4:
+// const inputMatrix = [[1, 2], [3, 4]]
+// Output: [[3,1],[4,2]]
+
 /**
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-const rotate = function (matrix) {
-    const n = matrix.length;
-    const n2 = Math.floor(n / 2);
+//* Time O(n), Space(1)
+//* Solution #1
+// const rotate = function (matrix) {
+//     const n = matrix.length;
+//     const n2 = Math.floor(n / 2);
 
-    for (let i = 0; i < n2; i++) {
-        for (let j = 0; j < n; j++) {
-            swap(matrix, i, j, n - 1 - i, j)
-        }
-    }
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            swap(matrix, i, j, j, i);
-        }
-    }
-}
+//     for (let i = 0; i < n2; i++) {
+//         for (let j = 0; j < n; j++) {
+//             swap(matrix, i, j, n - 1 - i, j)
+//             console.count("swaps 1");
+//             //* horizontal swap
+//             // 1 2 3     7 8 9
+//             // 4 5 6  => 4 5 6
+//             // 7 8 9     1 2 3
+//         }
+//     }
+//     for (let i = 0; i < n; i++) {
+//         for (let j = i + 1; j < n; j++) {
+//             swap(matrix, i, j, j, i);
+//             console.count("swaps 2");
+//             //* diagonal swap
+//             // 7 8 9     7 4 1
+//             // 4 5 6  => 8 5 2
+//             // 1 2 3     9 6 3
+//         }
+//     }
+// }
+// const swap = (matrix, x1, y1, x2, y2) => {
+//     const temp = matrix[x1][y1];
+//     matrix[x1][y1] = matrix[x2][y2];
+//     matrix[x2][y2] = temp;
+// }
 
-const swap = (matrix, x1, y1, x2, y2) => {
-    const temp = matrix[x1][y1];
-    matrix[x1][y1] = matrix[x2][y2];
-    matrix[x2][y2] = temp;
-}
+//* Solution #2
+// const rotate = function (matrix) {
+//     const n = matrix.length;
+//     for (let i = 0; i < Math.floor(n / 2); i++) {
+//         for (let j = 0; j < n / 2; j++) {
+//             let temp = matrix[n - 1 - j][i];
+//             matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+//             matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+//             matrix[j][n - 1 - i] = matrix[i][j];
+//             matrix[i][j] = temp;
+//         }
+//     }
+// }
 
-rotate(inputMatrix)
+//* Solution #3
+// const rotate = (matrix) => {
+//     const n = matrix.length
+//     transpose(matrix, n);
+//     // console.log("\ntranspose:", matrix)
+//     // reflect(matrix, n);
+//     // console.log("\nreflect:", matrix)
+//     return matrix
+// }
 
-console.log(inputMatrix)
+// function transpose(matrix, n) {
+//     console.log(matrix, n)
+//     for (let i = 0; i < n; i++) {
+//         for (let j = i; j < n; j++) {
+//             let temp = matrix[[j][i]];
+//             matrix[j][i] = matrix[i][j];
+//             matrix[i][j] = temp;
+
+//         }
+//     }
+//     console.log(matrix)
+// }
+
+// function reflect(matrix, n) {
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < Math.floor(n / 2); j++) {
+//             let temp = matrix[i][j];
+//             matrix[i][j] = matrix[i][n - j - 1];
+//             matrix[i][n - j - 1] = temp;
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+console.log("\n*******", rotate(inputMatrix))
